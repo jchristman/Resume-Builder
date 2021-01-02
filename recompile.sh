acf7dac3f054c7b0468e2e0607825be220c39c9d
brandonscholet <58888570+brandonscholet@users.noreply.github.com>
2021-01-02 09:53:16 -0500

    Update recompile.sh
    Not everyone has the open command natively

--- recompile.sh
+++ recompile.sh
@@ -3,4 +3,4 @@
 rm Resume.pdf
 docker run --rm -v `pwd`:/data christman-resume:latest make
 docker run --rm -v `pwd`:/data christman-resume:latest make clean
-open Resume.pdf
+$(which open xdg-open | head -n 1) Resume.pdf
