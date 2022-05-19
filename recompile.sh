f5663d9d58260e9a20615a0cd064ea43e91055aa
Brandon Scholet <bscholet@gmail.com>
2022-05-19 11:54:51 -0400

    fixing some dos2unix and open stuff

--- recompile.sh
+++ recompile.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-rm Resume.pdf
+rm -rf Resume.pdf
 docker run --rm -v `pwd`:/data christman-resume:latest make
 docker run --rm -v `pwd`:/data christman-resume:latest make clean
-$(which open xdg-open | head -n 1) Resume.pdf
